896f4d231ec98b8436431fb8874a54226409e6c9
Daniel Bachhuber <d@danielbachhuber.com>
2014-09-29 13:56:53 -0700

    Port over some of @johnbillion's code for displaying current branch

--- git-switch.php
+++ git-switch.php
@@ -26,6 +26,80 @@ class Git_Switch {
 	 */
 	private function load() {
 
+		add_action( 'admin_bar_menu', array( $this, 'action_admin_bar_menu' ), 999 );
+
+	}
+
+	/**
+	 * Display helpful details in the admin bar
+	 */
+	public function action_admin_bar_menu( $wp_admin_bar ) {
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
+		if ( ! $status = $this->get_git_status() ) {
+			return;
+		}
+
+		$wp_admin_bar->add_menu( array(
+			'id'     => 'git-switch',
+			'title'  => sprintf( '(%s)%s', $status['branch'], $status['dirty'] ),
+			'href'   => '#'
+		) );
+
+		$wp_admin_bar->add_menu( array(
+			'parent' => 'git-switch',
+			'id'     => 'git-switch-details',
+			'title'  => ( implode('', array_map( 'esc_html', $status['status'] ) ) ),
+			'href'   => '#'
+		) );
+
+	}
+
+	/**
+	 * Get the current Git status
+	 */
+	public function get_git_status() {
+
+		if ( ! function_exists( 'exec' ) ) {
+			return false;
+		}
+
+		$theme_path = get_stylesheet_directory();
+
+		exec( sprintf( 'cd %s; git status', escapeshellarg( $theme_path ) ), $status );
+
+		if ( empty( $status ) or ( false !== strpos( $status[0], 'fatal' ) ) ) {
+			return false;
+		}
+
+		$end = end( $status );
+		$return = array(
+			'dirty'  => '*',
+			'branch' => 'detached',
+			'status' => $status,
+			'remote' => array(),
+		);
+
+		if ( preg_match( '/On branch (.+)$/', $status[0], $matches ) ) {
+			$return['branch'] = trim( $matches[1] );
+		}
+
+		if ( empty( $end ) or ( false !== strpos( $end, 'nothing to commit' ) ) ) {
+			$return['dirty'] = '';
+		}
+
+		exec( sprintf( 'cd %s; git branch -r', escapeshellarg( $theme_path ) ), $branches );
+		if ( ! empty( $branches ) ) {
+			$branches = array_map( function( $branch ) {
+				return str_replace( 'origin/', '', $branch );
+			}, explode( PHP_EOL, $branches ) );
+			$return['remote'] = $branches;
+		}
+
+		return $return;
 	}
 
 }
