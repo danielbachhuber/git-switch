943dfe431abb38cdb842e4cebc872d23254935a0
Daniel Bachhuber <daniel@handbuilt.co>
2014-10-29 14:53:15 -0700

    Branch shouldn't ever be dirty when switching
    If it is, ignore it.

--- git-switch.php
+++ git-switch.php
@@ -67,7 +67,7 @@ class Git_Switch {
 
 		$theme_path = get_stylesheet_directory();
 
-		exec( sprintf( 'cd %s; git checkout %s', escapeshellarg( $theme_path ), escapeshellarg( $_GET['branch'] ) ), $results );
+		exec( sprintf( 'cd %s; git checkout -f %s', escapeshellarg( $theme_path ), escapeshellarg( $_GET['branch'] ) ), $results );
 		delete_transient( self::CACHE_KEY );
 		wp_safe_redirect( home_url() );
 		exit;
